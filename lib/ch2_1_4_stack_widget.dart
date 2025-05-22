f9666bb5de4ac5d2d62860f83b4af2c311f8af3d
jungbini <jungbini@gmail.com>
2025-05-22 14:13:46 +0000

    first commit

---
+++ lib/ch2_1_4_stack_widget.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// 1단계
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+// 2단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+          
+//         )
+//       )
+//     );
+//   }
+// }
+
+// 3단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Stack(
+//             children: <Widget>[
+//               Container(width: 300, height: 300, color: Colors.blue),
+//             ],
+//           )
+//         )        
+//       )
+//     );
+//   }
+// }
+
+// 4단계: Stack Widget
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Stack(
+//             children: <Widget>[
+//               Container(width: 300, height: 300, color: Colors.blue),
+//               Positioned(
+//                 top: 0,
+//                 left: 0,
+//                 child: Container(width: 100, height: 100, color: Colors.green),
+//               ),
+//               Positioned(
+//                 bottom: 0,
+//                 left: 0,
+//                 child: Container(width: 100, height: 100, color: Colors.red),
+//               ),
+//               Positioned(
+//                 top: 0,
+//                 right: 0,
+//                 child: Container(width: 100, height: 100, color: Colors.yellow),
+//               ),
+//               Positioned(
+//                 bottom: 0,
+//                 right: 0,
+//                 child: Container(width: 100, height: 100, color: Colors.orange),
+//               ),
+//             ],
+//           )
+//         )        
+//       )
+//     );
+//   }
+// }
+
+// 5단계: IndexedStack
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   int _selectedIndex = 0;
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: IndexedStack(
+//             index: _selectedIndex,
+//             children: <Widget>[
+              
+//             ],
+//           )
+//         )        
+//       )
+//     );
+//   }
+// }
+
+// 6단계
+// int _selectedIndex = 0;
+
+// class MyApp extends StatelessWidget {
+  
+//   const MyApp({super.key});
+ 
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: IndexedStack(
+//             index: _selectedIndex,
+//             children: <Widget>[
+//               Container(
+//                 color: Colors.red,
+//                 child: Center(
+//                   child: Text('첫 번째 페이지', style: TextStyle(color: Colors.white, fontSize: 24)),
+//                 ),
+//               ),
+//               Container(
+//                 color: Colors.green,
+//                 child: Center(
+//                   child: Text('두 번째 페이지', style: TextStyle(color: Colors.white, fontSize: 24)),
+//                 ),
+//               ),
+//               Container(
+//                 color: Colors.blue,
+//                 child: Center(
+//                   child: Text('세 번째 페이지', style: TextStyle(color: Colors.white, fontSize: 24)),
+//                 ),
+//               ),
+//             ],
+//           )
+//         )        
+//       )
+//     );
+//   }
+// }
---
+++ lib/ch2_2_1_image_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// 1단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+
+//         )
+//       )
+//     );
+//   }
+// }
+
+// 2단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Image(
+//             // pubspec.yaml 에 assets/images/ 폴더 등록이 필요함
+//             image: AssetImage('assets/images/pic.jpg'),
+//             // image: NetworkImage('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
+//           ),
+//         ),
+//       )
+//     );
+//   }
+// }
+
+// 3단계
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child:
+//             Image.asset('assets/images/pic.jpg'),
+//             // Image.network('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
+//         ),
+//       )
+//     );
+//   }
+// }
+
+// 4단계
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image.asset('assets/images/pic.jpg', fit: BoxFit.none),
+          // Image.network('https://pouriahosseini.gallerycdn.vsassets.io/extensions/pouriahosseini/omga-auto-commit/1.2.3/1741430181362/Microsoft.VisualStudio.Services.Icons.Default')
+        ),
+      ),
+    );
+  }
+}
